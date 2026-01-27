772cc2e21a3a160a825d42c018c9d37777dc60c2
rafaeleone26 <rafaeleone26@gmail.com>
2026-01-27 19:01:55 -0300

    better test logic

--- unit-testing-using-dotnet-test/CommandStreet.Tests/MovementTests.cs
+++ unit-testing-using-dotnet-test/CommandStreet.Tests/MovementTests.cs
@@ -7,25 +7,81 @@ public class MovementTests
     [Fact]
     public void Player_Wraps_Around_Board()
     {
-        Board board = new Board();
-        Player player = new Player("Test", 1000);
+        var board = new Board();
+        var player = new Player("Test", 1000);
 
         player.Position = 10;
+
+        Program.MovePlayer(player, board, roll: 3, totalSuits: 4, startBonus: 300);
+
+        Assert.Equal(1, player.Position);
+    }
+    
+    [Fact]
+    public void Player_Collects_Suit_When_Passing_Suit_Tile()
+    {
+        var board = new Board();
+        var player = new Player("Test", 1000);
+
+        player.Position = 9;
+
+        Program.MovePlayer(player, board, roll: 2, totalSuits: 4, startBonus: 300);
+
+        Assert.Single(player.Suits);
+    }
+
+    [Fact]
+    public void Player_Collects_Suit_When_Landing_Suit_Tile()
+    {
+        var board = new Board();
+        var player = new Player("Test", 1000);
+
+        player.Position = 9;
+
+        Program.MovePlayer(player, board, roll: 1, totalSuits: 4, startBonus: 300);
+
+        Assert.Single(player.Suits);
+    }
+
+    [Fact]
+        public void Player_Gets_Bonus_When_Passing_Start_With_All_Suits()
+    {
+        var board = new Board();
+        var player = new Player("Test", 1000);
+
+        player.Position = 10;
+
         player.Suits.Add(SuitType.Red);
         player.Suits.Add(SuitType.Blue);
         player.Suits.Add(SuitType.Green);
         player.Suits.Add(SuitType.Yellow);
 
-        Program.MovePlayer(
-            player: player,
-            board: board,
-            roll: 4,
-            totalSuits: 4,
-            startBonus: 300
-        );
+        int startingWallet = player.Wallet;
 
-        Assert.Equal(2, player.Position);
-        Assert.Equal(1, player.Suits.Count);
-        Assert.Equal(1300, player.Wallet);
+        Program.MovePlayer(player, board, roll: 3, totalSuits: 4, startBonus: 300);
+
+        Assert.Equal(startingWallet + 300, player.Wallet);
+        Assert.Empty(player.Suits);
+    }
+
+    [Fact]
+    public void Player_Gets_Bonus_When_Landing_Start_With_All_Suits()
+    {
+        var board = new Board();
+        var player = new Player("Test", 1000);
+
+        player.Position = 10;
+
+        player.Suits.Add(SuitType.Red);
+        player.Suits.Add(SuitType.Blue);
+        player.Suits.Add(SuitType.Green);
+        player.Suits.Add(SuitType.Yellow);
+
+        int startingWallet = player.Wallet;
+
+        Program.MovePlayer(player, board, roll: 2, totalSuits: 4, startBonus: 300);
+
+        Assert.Equal(startingWallet + 300, player.Wallet);
+        Assert.Empty(player.Suits);
     }
 }
